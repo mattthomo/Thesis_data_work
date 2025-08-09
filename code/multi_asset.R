2dafc76800480eb158623da2f2381103317bebb7
matthew_thomo <mthompsonwbhs@gmail.com>
2025-08-09 11:31:11 +0200

    Work from week of 4 August

--- code/multi_asset.R
+++ code/multi_asset.R
@@ -1,6 +1,9 @@
-# this function serves to create the wealth index to be used in the regression
+# this function creates a df of all asset value columns along with their corresponding binary ownership columns
 
-create_wealth_index <- function(df){
+multi_asset <- function(df){
+
+    asset_ownership_columns <- c("w1_a_ownrad_v", "w1_a_ownvehpri_v", "w1_a_ownmot_v", "w1_a_owncom_v", "w1_a_owncel_v","w1_a_ownrad", "w1_a_ownvehpri", "w1_a_ownmot", "w1_a_owncom", "w1_a_owncel")
+
 
     wealth_df <- df %>%
         filter(!is.na(w1_best_mthpid) & !is.na(w1_best_fthpid) |
@@ -14,12 +17,12 @@ create_wealth_index <- function(df){
         filter(!is.na(w1_best_fthpid))
 
 
-# now join info from main df (containing info on asset ownership) to this df of father pids
+    # now join info from main df (containing info on asset ownership) to this df of father pids
     join_fthdf <- father_pid_df %>%
         left_join(w1 %>%
                       rename("w1_best_fthpid" = "pid"),
                   by = "w1_best_fthpid") #%>%  # joining by the id of the father
-        #filter(w1_a_fthhh == 1) # only want those entries where pid stays with their father
+    #filter(w1_a_fthhh == 1) # only want those entries where pid stays with their father
 
     # create vector of all columns containing relevant asset ownership information (resale value of assets)
 
@@ -39,7 +42,7 @@ create_wealth_index <- function(df){
         left_join(w1 %>%
                       rename("w1_best_mthpid" = "pid"),
                   by = "w1_best_mthpid") #%>%  # joining by the id of the mother
-        #filter(w1_a_mthhh == 1) # only want those entries where pid stays with their mother
+    #filter(w1_a_mthhh == 1) # only want those entries where pid stays with their mother
 
     # just want info relevant to asset ownership of mother and ids
     join_mthdf <- join_mthdf %>%
@@ -81,28 +84,30 @@ create_wealth_index <- function(df){
 
     asset_df <- asset_df %>%
         mutate(nr_of_parents = rowSums(!is.na(across(c("w1_best_mthpid", "w1_best_fthpid"))))) %>%
-        mutate(nr_hh_memb = ifelse(is.na(w1_best_mthpid) & !is.na(w1_best_fthpid),
-                                   nr_of_parents + 1,
-                                   nr_of_parents + w1_a_bhlive_n)) %>%  # this creates 920 missing variables due to missing values for nr of kids
+        mutate(nr_hh_memb = ifelse(is.na(w1_best_mthpid) & !is.na(w1_best_fthpid), nr_of_parents + 1, nr_of_parents + w1_a_bhlive_n))
 
-    # bear with me as the following code all works to specify the nr of hh members of these 920 missings
+    final_df <- asset_df %>%
+        mutate(total_assets = rowSums(across(c("total_rad", "total_vehpriv", "total_mot", "total_com", "total_cel")))) %>%
+        mutate(total_assets_pc = total_assets/nr_hh_memb)
+
+    final_df
+}
 
 
-    # final_df <- asset_df %>%
-    #     mutate(total_assets = rowSums(across(c("total_rad", "total_vehpriv", "total_mot", "total_com", "total_cel")))) %>%
-    #     mutate(total_assets_pc = total_assets/nr_hh_memb)
+# join mom and dad asset values to target pop df
+
+multi_asset <- function(df){
+
+    asset_ownership_columns <- c("w1_a_ownrad_v", "w1_a_ownvehpri_v", "w1_a_ownmot_v", "w1_a_owncom_v", "w1_a_owncel_v","w1_a_ownrad", "w1_a_ownvehpri", "w1_a_ownmot", "w1_a_owncom", "w1_a_owncel")
+
+
+    asset_df <- df %>% # df = targetpop
+        left_join(w1_best %>% select(pid, w1_best_mthpid, w1_best_fthpid),
+                  by = "pid") %>% # add parents ids
+        left_join(w1 %>% select(all_of(asset_ownership_columns), pid) %>% rename("w1_best_mthpid" = "pid"), by = "w1_best_mthpid") %>%  # add mothers ownership data
+        left_join(w1 %>% select(all_of(asset_ownership_columns), pid) %>% rename("w1_best_fthpid" = "pid"), by = "w1_best_fthpid") # add fathers ownership data
 
     asset_df
 }
 
-join_test <- create_wealth_index(w1_best)
-
-join_test2 <- join_test %>%
-    filter(is.na(nr_hh_memb))
-
-join_test3 <- join_test2 %>%
-    mutate(parent_combo = paste(w1_best_mthpid, w1_best_fthpid, sep = "_")) %>%  # Combine parent IDs
-    group_by(parent_combo) %>%
-    mutate(n_siblings = n(),                            # total children with same parents
-           hh_members = nr_of_parents + n_siblings) %>%  # parents + siblings
-    ungroup()
+test_df <- multi_asset(targetpop)
