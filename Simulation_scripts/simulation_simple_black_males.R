6297e6518cba8ff2ae51ebdc4710b97ffa2e48d2
matthew_thomo <mthompsonwbhs@gmail.com>
2026-06-15 17:14:49 +0200

    Update thesis

--- Simulation_scripts/simulation_simple_black_males.R
+++ Simulation_scripts/simulation_simple_black_males.R
@@ -1,8 +1,25 @@
 # Main aim of this script is to have a more clean script showing my simulation results
 
-sim_function_reg <- function(par, n, target_coefs, weights){
+# Read in objects which will be used in function, which were created in Readme.rmd
+target_values_simple_black_males <- read_rds(file = "./output/target_black_males.rds")
+
+test_ols <- read_rds(file = "./output/test_ols.rds")
+
+observed_moments_df <- read_rds(file = "./output/observed_tab_black_males.rds")
+
+sim_ols_black_males <- read_rds(file = "./output/sim_ols_black_males.rds")
+
+educ_ols_simp_black_males <- read_rds(file = "./output/educ_ols_simp_black_males.rds")
 
 
+
+
+##### SIMULATION FUNCTION ############
+
+
+sim_function_reg <- function(par, n, target_coefs, weights, observed_moments_df){
+
+# these are the parameters
     params <- c(
         beta_0_educ   = par[1],
         alpha_educ    = par[2],
@@ -18,17 +35,13 @@ sim_function_reg <- function(par, n, target_coefs, weights){
 
     library(tidyverse)
 
-    #set.seed(123456)
+    set.seed(123456)
 
 
 
     #### Variables #####
 
-    # educ <- rnorm(n = 100, mean = 10, sd = 1)
-
     consc <- rnorm(n, mean = 0, sd = 1)
-
-
 
     ##### Parameters ####
 
@@ -103,8 +116,8 @@ sim_function_reg <- function(par, n, target_coefs, weights){
     comparison_df <- moments_df %>%
     bind_cols(sim_results)
 
-
-    observed_moments_df <- readRDS('/Users/matthewthompson/Documents/Stellenbosch University/Masters/Research Assignment/Data_work/output/observed_tab_black_males.rds')
+# read in df of observed moments from real data, constructed in README.rmd
+    observed_moments_df <- observed_moments_df
 
     loss_df <- comparison_df %>%
         bind_rows(observed_moments_df) %>%
@@ -113,7 +126,7 @@ sim_function_reg <- function(par, n, target_coefs, weights){
                      names_to = "Parameter",
                      values_to = "Value")  %>%  # new column gets made with all the names and is called "Parameter"
         pivot_wider(names_from = source,
-                    values_from = Value) # gets simmulated and observed data next to each other
+                    values_from = Value) # gets simulated and observed data next to each other
 
     loss_df
 
@@ -124,26 +137,6 @@ sim_function_reg <- function(par, n, target_coefs, weights){
     loss <- sum(weights * ((sim_values - target_values)^2))
 
     return(loss)
-    # comparison_df <- observed_moments_df %>%
-    #     bind_rows(sim_results) %>%
-    #     mutate(source = c("observed_data", "simulated_data")) %>%
-    #     pivot_longer(cols = -source,
-    #                  names_to = "parameter",
-    #                  values_to = "value") %>%
-    #     pivot_wider(names_from = source, values_from = value) %>%
-    #     mutate(across(where(is.numeric), ~ round(.x, 3))) %>%
-    #     mutate(diff = observed_data - simulated_data) %>%
-    #     mutate(squared_diff = diff^2) %>%
-    #     filter(parameter %in% c("inc_reg_intercept",
-    #                             "inc_reg_educ",
-    #                             "inc_reg_consc"))
-    #
-    # sim_coefs <- comparison_df$simulated_data
-    #
-    # sim_coefs
-    # loss <- sum(weights*(sim_coefs - target_coefs)^2)
-    # return(loss)
-
 
 }
 
@@ -221,9 +214,11 @@ obj_fun <- makeSingleObjectiveFunction(
         x <- as.numeric(x)
 
 
-        sim_function_reg(par = x, n = 100000,
-                              target_coefs = target_values_simple_black_males,
-                              weights = weights_vec)
+        sim_function_reg(par = x,
+                         n = 100000,
+                         target_coefs = target_values_simple_black_males,
+                         weights = weights_vec,
+                         observed_moments_df = observed_moments_df)
     },
     par.set = par_set_simple,
     minimize = TRUE
@@ -264,13 +259,27 @@ random_design <- generateRandomDesign(n = 50, par.set = par_set_simple)
 
 design_mat <- rbind(init_par, random_design)
 
+y_vals <- apply(design_mat, 1, function(row) {
+    sim_function_reg(
+        par          = as.numeric(row),
+        n            = 10000,
+        target_coefs = target_values_simple_black_males,
+        weights      = weights_vec,
+        observed_moments_df = observed_moments_df
+    )
+})
+
+# Add y column to design_mat
+design_mat$y <- y_vals
+
 # run in parallel
 parallelStartSocket(cpus = parallel::detectCores() - 1)
 
 parallelExport(
     "sim_function_reg",
     "target_values_simple_black_males",
-    "weights_vec"
+    "weights_vec",
+    "observed_moments_df"
 )
 
 # also load required packages on each worker
@@ -293,117 +302,16 @@ result$x        # optimal parameter values
 result$y        # final loss value
 
 
-# run at initial values
-test_loss <- sim_function_reg(
-    par          = as.numeric(unlist(init_par)),
-    n            = 100,
-    target_coefs = target_values_simple,
-    weights      = weights_vec
-)
-print(test_loss)
-
-# run at slightly different values
-test_loss2 <- sim_function_reg(
-    par          = as.numeric(unlist(init_par)) * 1.1,
-    n            = 100,
-    target_coefs = target_values_simple,
-    weights      = weights_vec
-)
-print(test_loss2)
-
-# if these are the same something is wrong in your function
-
 opt_path <- as.data.frame(result$opt.path)
 print(opt_path)
 
 # check if y values have any variation
 summary(opt_path$y)
-table(opt_path$y)  # if all same value, loss function is broken
-
-# Evaluate loss for each row of design_mat
-y_vals <- apply(design_mat, 1, function(row) {
-    sim_function_reg(
-        par          = as.numeric(row),
-        n            = 10000,
-        target_coefs = target_values_simple_black_males,
-        weights      = weights_vec
-    )
-})
-
-# Add y column to design_mat
-design_mat$y <- y_vals
-
-# Then pass to mbo
-result <- mbo(
-    fun       = obj_fun,
-    design    = design_mat,
-    control   = mbo_ctrl,
-    show.info = TRUE
-)
-
-result$x
-
-# Check for any NA or type problems introduced by rbind
-str(design_mat)
-anyNA(design_mat)
-
-design_mat <- rbind(init_par2, init_par3, init_par, random_design) %>%
-    mutate(across(everything(), as.numeric))
+table(opt_path$y)
 
 
-#### Design mat diagnosis #####
 
-# 1. Check design_mat looks correct
-str(design_mat)
-head(design_mat)
-anyNA(design_mat)
-
-# 2. Check y values in design_mat actually vary
-summary(design_mat$y)
-
-# 3. Check obj_fun works correctly by calling it directly
-# MBO passes parameters as a named list so replicate that exactly
-test1 <- obj_fun(list(
-    beta_0_educ   = 10,
-    alpha_educ    = 0.5,
-    beta_0_income = 5,
-    gamma_income  = 0.3,
-    alpha_income  = 0.2,
-    u_consc       = 0,
-    u_educ        = 0,
-    u_income      = 0
-))
-
-test2 <- obj_fun(list(
-    beta_0_educ   = 2,
-    alpha_educ    = 2,
-    beta_0_income = 2,
-    gamma_income  = 2,
-    alpha_income  = 2,
-    u_consc       = 1,
-    u_educ        = 1,
-    u_income      = 1
-))
-
-cat("obj_fun test 1:", test1, "\n")
-cat("obj_fun test 2:", test2, "\n")
-cat("Are they the same?", test1 == test2, "\n")
-
-# 4. Check opt.path after mbo run
-opt_path1 <- as.data.frame(result$opt.path)
-saveRDS(opt_path1, file = "./output/optpath1.rds")
-
-opt_path2 <- as.data.frame(result$opt.path)
-saveRDS(opt_path2, file = "./output/optpath2.rds")
-
-
-opt_path3 <- as.data.frame(result$opt.path)
-saveRDS(opt_path3, file = "./output/optpath3.rds")
-
-
-summary(opt_path$y)
-head(opt_path2[order(opt_path2$y), ], 10)  # top 10 best rows
-
+# try checking for local optimisers in the global optimisation function
 optim_result <- optim(
     par     = c(10, 0.5, 5, 0.3, 0.2, 0, 0, 0),  # rough starting guess
     fn      = function(par) {
@@ -411,22 +319,13 @@ optim_result <- optim(
             par          = par,
             n            = 10000,
             target_coefs = target_values_simple_black_males,
-            weights      = weights_vec
+            weights      = weights_vec,
+            observed_moments_df = observed_moments_df
         )
     },
     method  = "Nelder-Mead",
     control = list(maxit = 1000, reltol = 1e-8)
 )
-
-# Extract the best parameters found
-best_par <- optim_result$par
-print(best_par)
-print(optim_result$value)  # loss at these parameters
-
-init_par <- as.data.frame(t(best_par))
-colnames(init_par) <- c("beta_0_educ", "alpha_educ", "beta_0_income",
-                        "gamma_income", "alpha_income", "u_consc",
-                        "u_educ", "u_income")
 
 
 
@@ -458,7 +357,7 @@ init_par3 <- data.frame(
 
 # Try using a more narrow parameter space
 
-par_set_simple <- makeParamSet(
+par_set_simple_narrow <- makeParamSet(
     makeNumericParam("beta_0_educ",   lower = 0,  upper = 15),
     makeNumericParam("alpha_educ",    lower = -5,  upper = 5),
     makeNumericParam("beta_0_income", lower = 0,  upper = 15),
